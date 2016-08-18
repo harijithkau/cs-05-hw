696842cfac3059c2b3956d16fa991494ed4b77ac
harijithkau <harijith.p@kau.in>
2016-08-18 20:14:31 +0530

    Create display.asm

---
+++ display.asm
@@ -0,0 +1,20 @@
+;
+; Display the message Hello World
+;
+
+.model small
+
+.data 
+msg	DB 	"Hello World..!$"
+
+.code
+start: 
+	mov 	ax, @data
+	mov	ds, ax
+	lea	dx, msg
+	mov	ah, 09h
+	int	21h
+	mov	ah, 4ch
+	int 	21h
+end start
+
